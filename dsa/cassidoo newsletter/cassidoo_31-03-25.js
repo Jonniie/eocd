e9273906a69544a687d0fae9434e058b065c504f
Jonniie <jtech2096@gmail.com>
2025-03-31 08:39:32 +0100

    31/03/25 solution

--- dsa/cassidoo newsletter/cassidoo_31-03-25.js
+++ dsa/cassidoo newsletter/cassidoo_31-03-25.js
@@ -1,20 +1,20 @@
 /** Casiddoo Newsletter
-This week's question:
-Write a function that takes an array of timestamps (HH:MM) from the same day and returns the longest gap in minutes between consecutive timestamps.
+    This week's question:
+    Write a function that takes an array of timestamps (HH:MM) from the same day and returns the longest gap in minutes between consecutive timestamps.
 
-Examples:
+    Examples:
 
-findLongestTimeGap(['12:00'])
-0
+    findLongestTimeGap(['12:00'])
+    0
 
-findLongestTimeGap(['09:00', '11:00'])
-120
+    findLongestTimeGap(['09:00', '11:00'])
+    120
 
-findLongestTimeGap(['14:00', '09:00', '15:00', '10:30'])
-210
+    findLongestTimeGap(['14:00', '09:00', '15:00', '10:30'])
+    210
 
-findLongestTimeGap(['08:00', '10:00', '10:00', '14:00'])
-240
+    findLongestTimeGap(['08:00', '10:00', '10:00', '14:00'])
+    240
  */
 
 function findLongestTimeGap(arr) {
